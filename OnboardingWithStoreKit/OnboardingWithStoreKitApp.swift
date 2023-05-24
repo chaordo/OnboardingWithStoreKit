13d2a73301e3217272d0b049f5e0fb2209653169
quintus <john@chaordo.com>
2023-05-24 16:43:20 -0600

    Initial Commit

---
+++ OnboardingWithStoreKit/OnboardingWithStoreKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingWithStoreKitApp.swift
+//  OnboardingWithStoreKit
+//
+//  Created by John on 5/24/23.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingWithStoreKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
